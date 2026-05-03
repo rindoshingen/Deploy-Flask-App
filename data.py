be0c56b300906df27b10275a3f8cde1783096c59
Andrew Bris <zenjukai@gmail.com>
2026-05-03 17:20:23 -0600

    Add category to posts and books
    Added category field to posts and books.

--- data.py
+++ data.py
@@ -4,24 +4,28 @@ posts = [
         "title": "What is with All this Snow Today!",
         "content": "This is the content for this posting.",
         "date": "April 24, 2026",
+        "category": "Nature",
     },
     {
         "id": 2,
         "title": "Working with Flask",
         "content": "Rebuilding my blog to run off of Python & Flask.",
         "date": "April 1, 2026",
+        "category": "Science",
     },
     {
         "id": 3,
         "title": "Speak Rarely",
         "content": "Speak rarely. Speak clearly. Speak only what matters.",
         "date": "April 24, 2026",
+        "category": "Web Development",
     },
     {
         "id": 4,
         "title": "Kanjizai",
         "content": "(kan) the true reality (zai) of the self (ji) as life. Kanjizai Bosatsu is not someone outside us. Each one of us must be Kanjizai Bosatsu. All of us, without exception, are actually living out the self that is only the self. – Shohaku Okumura",
         "date": "Feburary 10, 2026",
+        "category": "Hiking",
     },
 ]
 
